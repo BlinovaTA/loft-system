e4ad02e5a5643510026ec6d5feb734c04789dee6
BlinovaTA <tanyushablinova@gmail.com>
2021-07-31 14:52:03 +0500

    Работа с пользователями

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,5 +6,6 @@ router.use('/registration', require('./registration'))
 router.use('/refresh-token', require('./refresh-token'))
 router.use('/profile', require('./profile'))
 router.use('/news', require('./news'))
+router.use('/users', require('./users'))
 
 module.exports = router
