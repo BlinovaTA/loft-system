05dda3ffe42ff3fc873df62c9ebe16874935a45b
BlinovaTA <tanyushablinova@gmail.com>
2021-08-03 17:24:36 +0500

    frontend build

---
+++ server/client-build/precache-manifest.ec65dcd8c4d84c3d29cd128a297aba9a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77745f9364e2ba5d85157acf72316298",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffb50b527366fa4b62d3",
+    "url": "/static/js/2.c56d7368.chunk.js"
+  },
+  {
+    "revision": "fc8d9a9543fa8de9527d",
+    "url": "/static/js/main.03ee4d64.chunk.js"
+  },
+  {
+    "revision": "baefef36d43e832e04d7",
+    "url": "/static/js/runtime~main.f331c780.js"
+  }
+]);
